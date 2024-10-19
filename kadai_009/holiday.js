150843f206f281fddaaa9ca224a3aba14852f5f1
yukiko0606 <arashi.stf.0606@gmail.com>
2024-10-19 09:35:45 +0900

    課題9章提出

---
+++ kadai_009/holiday.js
@@ -0,0 +1,13 @@
+const holidays = ["元日", "成人の日", "建国記念の日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"];
+
+// for分の場合
+for(let i= 0; i < holidays.length; i++){
+    console.log(holidays[i]);
+}
+
+// whileの場合
+let conut = 0;
+while(holidays[conut] !== undefined){
+    console.log(holidays[conut]);
+    conut++;
+}
